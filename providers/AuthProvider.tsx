7b40b8394bb432e04c4733d5e37145c89ed4bf6b
uma <yadavuma419@gmail.com>
2023-09-13 09:35:17 +0530

    provider changed

--- providers/AuthProvider.tsx
+++ providers/AuthProvider.tsx
@@ -1,6 +1,6 @@
 "use client"
 import React, { ReactNode } from 'react'
-import { SessionProvider } from 'next-auth/react'
+import { SessionProvider } from "next-auth/react"
 const AuthProvider = ({ children }: { children: ReactNode }) => {
     return <>
         <SessionProvider>
